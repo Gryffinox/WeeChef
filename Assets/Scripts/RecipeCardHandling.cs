97c253c12d7392cdda057d3a8387c2a749f9cc59
KaiWasher <kaiwasher1998@gmail.com>
2021-12-03 01:35:23 -0500

    player recipe menu shows correct recipes of active player

--- Assets/Scripts/RecipeCardHandling.cs
+++ Assets/Scripts/RecipeCardHandling.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class RecipeCardHandling : MonoBehaviour
 {
     private Player player;
+    private Player nextPlayer;
 
     private List<RecipeCard> playerRecipeCards;
 
@@ -24,12 +25,37 @@ public class RecipeCardHandling : MonoBehaviour
         player = PlayerParent.GetActivePlayer();
         playerRecipeCards = player.GetRecipeHand();
 
-        for (int i = 0; i < player.GetRecipeHand().Count; i++)
+        if (player != nextPlayer)
         {
-            if (recipeCardsInMenu[i].getRecipeName() == null)
+            for (int i = 0; i < 4; i++)
             {
+                recipeCardsInMenu[i].SetRecipeName("no recipe");
+                recipeCardsInMenu[i].SetRecipeType("no recipe");
+                recipeCardsInMenu[i].SetIngredients(null);
+                recipeCardsInMenu[i].SetIngredientIds(null);
+                recipeCardText[i].text = "No Recipe";
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (playerRecipeCards.Count == 0)
+            {
+                recipeCardsInMenu[i].SetRecipeName("no recipe");
+                recipeCardsInMenu[i].SetRecipeType("no recipe");
+                recipeCardsInMenu[i].SetIngredients(null);
+                recipeCardsInMenu[i].SetIngredientIds(null);
+                recipeCardText[i].text = "No Recipe";
+            }
+            else
+            {
+                recipeCardsInMenu[i].SetRecipeName(playerRecipeCards[i].getRecipeName());
+                recipeCardsInMenu[i].SetRecipeType(playerRecipeCards[i].getRecipeType());
+                recipeCardsInMenu[i].SetIngredients(playerRecipeCards[i].getIngredientsOfRecipeList());
+                recipeCardsInMenu[i].SetIngredientIds(playerRecipeCards[i].getIngredientIds());
                 recipeCardText[i].text = playerRecipeCards[i].ToString();
             }
         }
+        nextPlayer = PlayerParent.GetActivePlayer();
     }   
 }
